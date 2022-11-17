8a7d6d8b0d6eecb7634f7db6959aae5c2b787f00
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-17 16:37:56 +0100

    Completed logic

--- logic.js
+++ logic.js
@@ -1,12 +1,4 @@
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta (1-100): le bombe;
-// Nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali;
-
-// In seguito l’utente clicca su una cella:
-   // se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina
-   // altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle;
-// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe);
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba;
-
+// CAMPO MINATO
 
 
 // bottone
@@ -15,7 +7,9 @@ const buttonGrill = document.getElementById("but-grill");
 const grill = document.getElementById("grill");
 // Click cell
 let clickCell = true;
-
+// Array num normal cell
+const normCellArr = [];
+console.log(normCellArr);
 
 // creare Griglia e farla comparire solo al Click del button
 buttonGrill.addEventListener("click",
@@ -30,28 +24,40 @@ buttonGrill.addEventListener("click",
             grill.append(cell);
             cell.innerHTML = i;
             
+
             // quando user clicca una cella, questa si colora di rosso se è bomba, altrimenti di azzurro
-            if (clickCell === true){
-                cell.addEventListener("click",
-                    function(){
+            cell.addEventListener("click",
+                function(){
+                    // controllare se è true o no. Se true c'è evento, altrimenti no
+                    if (clickCell === true){
+
+                        // num cell cliccata
+                        console.log("Cell num:", i);
 
                         if (bombArr.includes(i)){
-                            cell.classList.add("cell-bomb"); //red
-                            clickCell = false;
+                            cell.classList.add("cell-bomb");
+                            clickCell = false; //assegna false a let clickCell, quindi non ci potrà più essere l'evento post click
+                            console.log("Hai trovato una bomba, hai preso!");
                         } else {
-                            cell.classList.add("cell-normal"); //skyblue
-                            clickCell = true;
+                            cell.classList.add("cell-normal");
+                            clickCell = true; //riassegna false a let clickCell, quindi ci potrà essere ancora l'evento post click
+                            normCellArr.push(i);
                         }
 
-                        // emettere un messaggio in console con il numero della cella cliccata
-                        console.log("Cell num:", i);
+                        // raggiunto num max possibile di numeri normali, partita finita
+                        if (normCellArr.length === 100 - 16){
+                            clickCell = false;
+                            console.log("Non hai trovato bombe, hai vinto!");
+                        }
+
+                        // comunicare il punteggio, cioè num volte che user ha cliccato una cella che non era bomba
+                        console.log("Punteggio:", normCellArr.length);
                     }
-                )
-            }
+                }
+            )
         }
     }
 )
-
 
 // Generare 16 numeri casuali nel range 1-100: le Bombe
 const bombArr = [];
